2e653ebc80e2cfd0821f2c7caf91a1854d51a545
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-02-19 19:06:01 +0100

    sync

--- gempyor_logprob.py
+++ gempyor_logprob.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 import datetime
 import glob, os, sys, re, shutil
 from pathlib import Path
+import numba as nb
 
 # import seaborn as sns
 import os
 import pandas as pd
 from gempyor import config, outcomes, seir
+import scipy
 
 
 
@@ -26,7 +28,6 @@ def run_simulation(snpi_df_in, hnpi_df_in, modinf, p_draw, unique_strings, trans
                     config=config,
                     bypass_DF=hnpi_df_in
                 )
-        
 
     # reduce them
     parameters = modinf.parameters.parameters_reduce(p_draw, npi_seir)
@@ -35,6 +36,13 @@ def run_simulation(snpi_df_in, hnpi_df_in, modinf, p_draw, unique_strings, trans
         parameters, modinf.parameters.pnames, unique_strings
     )
 
+    seeding_data_nbdict = nb.typed.Dict.empty(
+        key_type=nb.types.unicode_type,
+        value_type=nb.types.int64[:])
+
+    for k,v in seeding_data.items():
+        seeding_data_nbdict[k] = np.array(v, dtype=np.int64)
+
     states = seir.steps_SEIR(
         modinf,
         parsed_parameters,
@@ -42,7 +50,7 @@ def run_simulation(snpi_df_in, hnpi_df_in, modinf, p_draw, unique_strings, trans
         proportion_array,
         proportion_info,
         initial_conditions,
-        seeding_data,
+        seeding_data_nbdict,
         seeding_amounts,
     )
 
@@ -77,21 +85,19 @@ def compute_likelyhood(model_df, gt, modinf, statistics):
         model_df_s = model_df[model_df["subpop"]==subpop].drop(["subpop","time"],axis=1).loc[min(gt_s.index):max(gt_s.index)].resample("W-SAT").sum() # todo sub subpop here
         for key, value in statistics.items():
             assert model_df_s[key].shape == gt_s[value].shape
-            log_loss += sum(np.log((model_df_s[key]-gt_s[value])**2))
 
-    return log_loss
+            log_loss += sum((model_df_s[key]-gt_s[value])**2)
+
+    return -log_loss
 
 
 def check_in_bounds(proposal, fitted_params):
-    proposal = np.random.random(len(fitted_params["ptype"]))*1.5
     if (proposal < fitted_params["lb"]).any() or (proposal > fitted_params["ub"]).any():
         return False
     return True
 
-def log_prob(proposal, snpi_df_ref, ndim, statistics, fitted_params, gt, hnpi_df_ref, modinf, p_draw, unique_strings, transition_array, proportion_array, proportion_info, initial_conditions, seeding_data, seeding_amounts,outcomes_parameters):
-    if not check_in_bounds(proposal=proposal, fitted_params=fitted_params):
-        return np.inf
-    
+
+def input_proposal(proposal, snpi_df_ref, hnpi_df_ref, fitted_params, ndim):
     snpi_df_mod = snpi_df_ref.copy(deep=True)
     hnpi_df_mod = hnpi_df_ref.copy(deep=True)
 
@@ -101,11 +107,24 @@ def log_prob(proposal, snpi_df_ref, ndim, statistics, fitted_params, gt, hnpi_df
         elif fitted_params["ptype"][i] == "hnpi":
             hnpi_df_mod.loc[hnpi_df_mod["npi_name"] == fitted_params["pname"][i],"reduction"] = proposal[i]
 
+    return snpi_df_mod, hnpi_df_mod
+
+
+
+
+def log_prob(proposal, snpi_df_ref, ndim, statistics, fitted_params, gt, hnpi_df_ref, modinf, p_draw, unique_strings, transition_array, proportion_array, proportion_info, initial_conditions, seeding_data, seeding_amounts,outcomes_parameters):
+    if not check_in_bounds(proposal=proposal, fitted_params=fitted_params):
+        print("OUT OF BOUND!!")
+        return -np.inf
+    
+    snpi_df_mod, hnpi_df_mod = input_proposal(proposal, snpi_df_ref, hnpi_df_ref, fitted_params, ndim)
+
     outcomes_df = run_simulation(snpi_df_mod, 
                                 hnpi_df_mod,
                                 modinf, p_draw, unique_strings, transition_array, proportion_array, proportion_info, initial_conditions, seeding_data, seeding_amounts,outcomes_parameters)
 
     llik = compute_likelyhood(model_df=outcomes_df, gt=gt, modinf=modinf, statistics=statistics)
+    print(f"llik is {llik}")
 
     return llik
 
