61ac4907ff38b11da282ba81ad510d5e0f54178c
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-04-03 01:23:51 +0200

    first scripts

---
+++ emcee_rsv_128.py
@@ -0,0 +1,294 @@
+# %%
+import gempyor
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import glob, os, sys, re, shutil
+from pathlib import Path
+
+# import seaborn as sns
+import matplotlib._color_data as mcd
+import pyarrow.parquet as pq
+import click
+import subprocess
+import dask.dataframe as dd
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
+from matplotlib.backends.backend_pdf import PdfPages
+
+import os
+import multiprocessing as mp
+import pandas as pd
+import pyarrow.parquet as pq
+from gempyor import config, model_info, outcomes, seir
+from multiprocessing import cpu_count
+import emcee
+
+from multiprocessing import Pool
+from gempyor_logprob import log_prob, compute_likelyhood, check_in_bounds, run_simulation, input_proposal
+
+import os
+
+# disable  operations using the MKL linear algebra.
+os.environ["OMP_NUM_THREADS"] = "1"
+
+
+data_dir = "RSV_USA/"
+
+os.chdir(data_dir)
+data_dir = "."  # necessary for now for time serie parameters !!! fixme 
+
+# %%
+config_path = f"config_CACOMD_maternal_v8_emcee.yml"
+nwalkers = 128
+niter = 400
+nsamples = 200 # number of likelyhood eval to write to disk...
+
+ncpu = cpu_count()
+print(f"found {ncpu} CPUs, using these")
+
+
+# %%
+
+run_id = config_path.split(".")[0]
+
+config.clear()
+config.read(user=False)
+config.set_file(config_path)
+print(config_path)
+
+
+seir_modifiers_scenario="none"
+outcome_modifiers_scenario="IHRadj"
+
+in_run_id = run_id
+out_run_id = in_run_id
+in_prefix = f"emcee_{run_id}"
+
+out_prefix = in_prefix
+
+write_csv = False
+write_parquet = True
+modinf = model_info.ModelInfo(
+    config=config,
+    nslots=1,
+    seir_modifiers_scenario=seir_modifiers_scenario,
+    outcome_modifiers_scenario=outcome_modifiers_scenario,
+    write_csv=write_csv,
+    write_parquet=write_parquet,
+    first_sim_index=1,
+    in_run_id=in_run_id,
+    in_prefix=in_prefix,
+    inference_filename_prefix="no",
+    inference_filepath_suffix="no",
+    out_run_id=out_run_id,
+    out_prefix=out_prefix,
+    stoch_traj_flag=False,
+)
+
+nsubpop = len(modinf.subpop_struct.subpop_names)
+subpop_names = modinf.subpop_struct.subpop_names
+
+# %%
+# find what to perturb
+fitted_params = {
+    "ptype":[],
+    "pname":[],
+    "subpop":[],
+    "pdist":[],
+    "ub":[],
+    "lb":[],
+}
+ndim=0
+
+print(f"there are {nsubpop} subpop in the config")
+
+print("SEIR MODIFIERS")
+for npi in gempyor.config["seir_modifiers"]["modifiers"].get():
+    if gempyor.config["seir_modifiers"]["modifiers"][npi]["perturbation"].exists():
+        c = config["seir_modifiers"]["modifiers"][npi]
+        for sp in modinf.subpop_struct.subpop_names:
+            fitted_params["ptype"].append("snpi")
+            fitted_params["pname"].append(npi)
+            fitted_params["subpop"].append(sp)
+            fitted_params["pdist"].append(c["value"].as_random_distribution())
+            fitted_params["lb"].append(c["value"]["a"].get())
+            fitted_params["ub"].append(c["value"]["b"].get())
+            ndim+=1
+        print(f" >> {npi} has perturbation, recording")
+        
+print("OUTCOMES MODIFIERS")   
+for npi in gempyor.config["outcome_modifiers"]["modifiers"].get():
+    if gempyor.config["outcome_modifiers"]["modifiers"][npi]["perturbation"].exists():
+        c = config["outcome_modifiers"]["modifiers"][npi]
+        for sp in modinf.subpop_struct.subpop_names:
+            fitted_params["ptype"].append("hnpi")
+            fitted_params["pname"].append(npi)
+            fitted_params["subpop"].append(sp)
+            fitted_params["pdist"].append(c["value"].as_random_distribution())
+            fitted_params["lb"].append(c["value"]["a"].get())
+            fitted_params["ub"].append(c["value"]["b"].get())
+            ndim+=1
+        print(f" >> {npi} has perturbation, recording")
+
+
+# TODO: does not support the subpop groups here !!!!!!!
+print(f"The dimension of the parameter space is {ndim}!!")
+
+# %%
+# Find the ground-truth
+gt = pd.read_csv(f"{data_dir}/"+gempyor.config["inference"]["gt_data_path"].get())
+# gt
+statistics = {}
+# # Ingoring agreegation and all, assuming by weekP
+for stat in gempyor.config["inference"]["statistics"]:
+    statistics[gempyor.config["inference"]["statistics"][stat]["sim_var"].get()] = gempyor.config["inference"]["statistics"][stat]["data_var"].get()
+statistics
+gt = gempyor.read_df(gempyor.config["inference"]["gt_data_path"].get())
+gt["date"]= pd.to_datetime(gt['date'])
+gt = gt.set_index("date")
+
+# %%
+
+
+# %% [markdown]
+# ## Create the first gempyor object
+
+# %%
+
+print("MAKING A TEST RUN TO GET SETUP")
+(
+    unique_strings,
+    transition_array,
+    proportion_array,
+    proportion_info,
+) = modinf.compartments.get_transition_array()
+
+outcomes_parameters = outcomes.read_parameters_from_config(modinf)
+
+
+npi_seir = seir.build_npi_SEIR(
+    modinf=modinf, load_ID=False, sim_id2load=None, config=config
+)
+if modinf.npi_config_outcomes:
+    npi_outcomes = outcomes.build_outcome_modifiers(
+                modinf=modinf,
+                load_ID=False,
+                sim_id2load=None,
+                config=config,
+            )
+
+p_draw = modinf.parameters.parameters_quick_draw(
+                n_days=modinf.n_days, nsubpops=modinf.nsubpops
+            )
+
+initial_conditions = modinf.initial_conditions.get_from_config(sim_id=0, setup=modinf)
+seeding_data, seeding_amounts = modinf.seeding.get_from_config(sim_id=0, setup=modinf)
+
+
+
+# reduce them
+parameters = modinf.parameters.parameters_reduce(p_draw, npi_seir)
+        # Parse them
+parsed_parameters = modinf.compartments.parse_parameters(
+    parameters, modinf.parameters.pnames, unique_strings
+)
+
+states = seir.steps_SEIR(
+    modinf,
+    parsed_parameters,
+    transition_array,
+    proportion_array,
+    proportion_info,
+    initial_conditions,
+    seeding_data,
+    seeding_amounts,
+)
+
+seir_out_df_ref = seir.postprocess_and_write(
+    0, modinf, states, p_draw, npi_seir, seeding_data
+)
+snpi_df_ref = npi_seir.getReductionDF()
+
+outcomes_df, hpar_df = outcomes.compute_all_multioutcomes(
+    modinf=modinf,
+    sim_id2write=0,
+    parameters=outcomes_parameters,
+    loaded_values=None,
+    npi=npi_outcomes,
+    bypass_seir=seir_out_df_ref
+)
+outcomes_df_ref, hpar_ref, hnpi_df_ref = outcomes.postprocess_and_write(
+    sim_id=0,
+    modinf=modinf,
+    outcomes_df=outcomes_df,
+    hpar=hpar_df,
+    npi=npi_outcomes,
+)
+
+outcomes_df_ref = outcomes_df_ref.set_index("date")
+print("TEST RUN IS DONE")
+
+
+# %%
+# need to convert to numba dict to python dict so it is pickable
+seeding_data = dict(seeding_data)
+
+# %%
+if False:
+    outcomes_df = run_simulation(snpi_df_in=snpi_df_ref, hnpi_df_in=hnpi_df_ref)
+    outcomes_df_ref.set_index("date")["incidH"].plot(lw=2, c='k')
+    outcomes_df.set_index("date")["incidH"].plot(lw=1, c='r')
+
+
+# %%
+
+# find the initial point from the chain:
+p0 = np.zeros((nwalkers, ndim))
+for i in range(ndim):
+    p0[:,i] = fitted_params["pdist"][i](nwalkers)
+    # DOES not take subpop groups into consideration !!!!!!
+
+
+print(f"initial run llik {compute_likelyhood(outcomes_df_ref, gt, modinf, statistics)}")
+
+
+# %%
+for i in range(nwalkers):
+    assert check_in_bounds(p0[i], fitted_params)
+
+# %%
+
+
+# %%
+filename = f"{run_id}_backend.h5"
+backend = emcee.backends.HDFBackend(filename)
+backend.reset(nwalkers, ndim)
+
+with Pool(ncpu) as pool:
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, 
+                                    args=[snpi_df_ref, ndim, statistics, fitted_params, gt, hnpi_df_ref, modinf, p_draw, unique_strings, transition_array, proportion_array, proportion_info, initial_conditions, seeding_data, seeding_amounts,outcomes_parameters, False], 
+                                    pool=pool,
+                                    backend=backend, moves=[(emcee.moves.StretchMove(live_dangerously=True), 1)])
+    state = sampler.run_mcmc(p0, niter, progress=True, skip_initial_state_check=True)
+
+print("done emcee, doing sampling")
+# %%
+backend = emcee.backends.HDFBackend(filename)
+with Pool(nwalkers) as pool:
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, 
+                                    args=[snpi_df_ref, ndim, statistics, fitted_params, gt, hnpi_df_ref, modinf, p_draw, unique_strings, transition_array, proportion_array, proportion_info, initial_conditions, seeding_data, seeding_amounts,outcomes_parameters, True], 
+                                    pool=pool,
+                                    backend=backend,  moves=[(emcee.moves.StretchMove(live_dangerously=True), 1)])
+    state = sampler.run_mcmc(None, (nsamples//nwalkers)+1, progress=True, skip_initial_state_check=True)
+
+# %%
+
+
+# %%
+print(
+    "Mean acceptance fraction: {0:.3f}".format(
+        np.mean(sampler.acceptance_fraction)
+    )
+)
