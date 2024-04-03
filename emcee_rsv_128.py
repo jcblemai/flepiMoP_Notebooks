26ac33624b64627069e49f83c437b9a2ea349332
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-04-03 18:38:34 +0200

    ready for new runs

--- emcee_rsv_128.py
+++ emcee_rsv_128.py
@@ -1,4 +1,3 @@
-# %%
 import gempyor
 import numpy as np
 import pandas as pd
@@ -28,22 +27,18 @@ import emcee
 from multiprocessing import Pool
 from gempyor_logprob import log_prob, compute_likelyhood, check_in_bounds, run_simulation, input_proposal
 
-import os
-
 # disable  operations using the MKL linear algebra.
 os.environ["OMP_NUM_THREADS"] = "1"
-
-
 data_dir = "RSV_USA/"
-
 os.chdir(data_dir)
 data_dir = "."  # necessary for now for time serie parameters !!! fixme 
 
 # %%
 config_path = f"config_CACOMD_maternal_v8_emcee.yml"
 nwalkers = 128
-niter = 400
-nsamples = 200 # number of likelyhood eval to write to disk...
+niter = 100
+nsamples = 100 # number of likelyhood eval to write to disk...
+thin=5
 
 ncpu = cpu_count()
 print(f"found {ncpu} CPUs, using these")
@@ -226,7 +221,8 @@ outcomes_df_ref, hpar_ref, hnpi_df_ref = outcomes.postprocess_and_write(
     hpar=hpar_df,
     npi=npi_outcomes,
 )
-
+outcomes_df_ref["time"] = outcomes_df_ref["date"] #which one should it be ?
+modinf.write_simID(ftype="hosp", sim_id=0, df=outcomes_df_ref)
 outcomes_df_ref = outcomes_df_ref.set_index("date")
 print("TEST RUN IS DONE")
 
@@ -234,13 +230,6 @@ print("TEST RUN IS DONE")
 # %%
 # need to convert to numba dict to python dict so it is pickable
 seeding_data = dict(seeding_data)
-
-# %%
-if False:
-    outcomes_df = run_simulation(snpi_df_in=snpi_df_ref, hnpi_df_in=hnpi_df_ref)
-    outcomes_df_ref.set_index("date")["incidH"].plot(lw=2, c='k')
-    outcomes_df.set_index("date")["incidH"].plot(lw=1, c='r')
-
 
 # %%
 
@@ -267,7 +256,7 @@ backend = emcee.backends.HDFBackend(filename)
 backend.reset(nwalkers, ndim)
 
 with Pool(ncpu) as pool:
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, 
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob,
                                     args=[snpi_df_ref, ndim, statistics, fitted_params, gt, hnpi_df_ref, modinf, p_draw, unique_strings, transition_array, proportion_array, proportion_info, initial_conditions, seeding_data, seeding_amounts,outcomes_parameters, False], 
                                     pool=pool,
                                     backend=backend, moves=[(emcee.moves.StretchMove(live_dangerously=True), 1)])
@@ -275,20 +264,89 @@ with Pool(ncpu) as pool:
 
 print("done emcee, doing sampling")
 # %%
-backend = emcee.backends.HDFBackend(filename)
-with Pool(nwalkers) as pool:
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, 
-                                    args=[snpi_df_ref, ndim, statistics, fitted_params, gt, hnpi_df_ref, modinf, p_draw, unique_strings, transition_array, proportion_array, proportion_info, initial_conditions, seeding_data, seeding_amounts,outcomes_parameters, True], 
-                                    pool=pool,
-                                    backend=backend,  moves=[(emcee.moves.StretchMove(live_dangerously=True), 1)])
-    state = sampler.run_mcmc(None, (nsamples//nwalkers)+1, progress=True, skip_initial_state_check=True)
 
-# %%
+last_llik = sampler.get_log_prob()[-1,:]
+good_slots = last_llik > (last_llik.mean()-1*last_llik.std())
+print(f"there are {good_slots.sum()}/{len(good_slots)} good walkers... keeping these")
+
+fig, axes = plt.subplots(ndim+1,2, figsize=(15, (ndim+1)*2))
+
+labels = list(zip(fitted_params["pname"], fitted_params["subpop"]))
+samples = sampler.get_chain()
+
+import seaborn as sns
+def plot_chain(frompt,axes):
+    ax = axes[0]
+
+    ax.plot(np.arange(frompt,frompt+sampler.get_log_prob()[frompt:].shape[0]),
+                      sampler.get_log_prob()[frompt:,good_slots], "navy", alpha=.2, lw=1, label="good walkers")
+    ax.plot(np.arange(frompt,frompt+sampler.get_log_prob()[frompt:].shape[0]),
+            sampler.get_log_prob()[frompt:,~good_slots], "tomato", alpha=.4, lw=1, label="bad walkers")
+    ax.set_title("llik")
+    #ax.legend()
+    sns.despine(ax=ax, trim=False)
+    ax.set_xlim(frompt, frompt+sampler.get_log_prob()[frompt:].shape[0])
+
+    #ax.set_xlim(0, len(samples))
+
+    for i in range(ndim):
+        ax = axes[i+1]
+        ax.plot(np.arange(frompt,frompt+sampler.get_log_prob()[frompt:].shape[0]),
+                samples[frompt:,good_slots, i], "navy", alpha=.2, lw=1,)
+        ax.plot(np.arange(frompt,frompt+sampler.get_log_prob()[frompt:].shape[0]),
+                samples[frompt:, ~good_slots, i], "tomato", alpha=.4, lw=1,)
+        #ax.set_xlim(0, len(samples))
+        ax.set_title(labels[i])
+        #ax.yaxis.set_label_coords(-0.1, 0.5)
+        sns.despine(ax=ax, trim=False)
+        ax.set_xlim(frompt, frompt+samples[frompt:].shape[0])
+        
+
+    axes[-1].set_xlabel("step number");
+
+plot_chain(0,axes[:,0])
+plot_chain(3*samples.shape[0]//4,axes[:,1])
+fig.tight_layout()
+
+plt.savefig(f"{run_id}_chains.pdf")
+
+good_samples =  sampler.get_chain()[:,good_slots,:]
+
+step_number = -1
+exported_samples = np.empty((nsamples,ndim))
+for i in range(nsamples):
+    exported_samples[i,:] = good_samples[step_number - thin*(i//(good_slots.sum())) ,i%(good_slots.sum()),:] # parentesis around i//(good_slots.sum() are very important
+
+position_arguments = [snpi_df_ref, ndim, statistics, fitted_params, gt, hnpi_df_ref, modinf, p_draw, unique_strings, transition_array, proportion_array, proportion_info, initial_conditions, seeding_data, seeding_amounts,outcomes_parameters, True]
+
+with Pool(ncpu) as pool:
+    results = pool.starmap(log_prob, [(sample, *position_arguments) for sample in exported_samples])
 
 
-# %%
-print(
-    "Mean acceptance fraction: {0:.3f}".format(
-        np.mean(sampler.acceptance_fraction)
-    )
-)
+results = []
+for fn in gempyor.utils.list_filenames(folder="model_output/", filters=[run_id,"hosp.parquet"]):
+
+    df = gempyor.read_df(fn)
+    #raise ValueError
+    df["date"] = df["time"]
+    df = df.set_index("date")
+    results.append(df)
+
+fig, axes = plt.subplots(len(statistics),len(subpop_names), figsize=(5*len(subpop_names), 3*len(statistics)), sharex=True, dpi=300)
+for j, subpop in enumerate(modinf.subpop_struct.subpop_names):
+        gt_s = gt[gt["subpop"]==subpop].sort_index()
+        first_date = max(gt_s.index.min(),results[0].index.min())
+        last_date = min(gt_s.index.max(), results[0].index.max())
+        gt_s = gt_s.loc[first_date:last_date].drop(["subpop"],axis=1).resample("W-SAT").sum()
+        
+        for i, (key, value) in enumerate(statistics.items()):
+                ax = axes[i,j]
+                ax.plot(gt_s[value], color='k', marker='.', lw=1)
+                for model_df in results:
+                        model_df_s = model_df[model_df["subpop"]==subpop].drop(["subpop","time"],axis=1).loc[first_date:last_date].resample("W-SAT").sum() # todo sub subpop here
+                        ax.plot(model_df_s[key],  lw=.9, alpha=.5)
+                #if True:
+                #        init_df_s = outcomes_df_ref[model_df["subpop"]==subpop].drop(["subpop","time"],axis=1).loc[min(gt_s.index):max(gt_s.index)].resample("W-SAT").sum() # todo sub subpop here
+                ax.set_title(f"{value}, {subpop}")
+fig.tight_layout()
+plt.savefig(f"{run_id}_results.pdf")
